1ee715bb6bda1fe1805f59a9bc716b6f8760fdaa
Haukaun <haakonfs@hotmail.com>
2023-10-02 21:26:11 +0200

    Added shadcn config and button test

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Button } from "./components/ui/button";
+
 function App() {
 	return (
 		<div className="h-screen grid place-items-center">
-			<h1 className="text-8xl">
+			<h1 className="text-8xl flex flex-col ">
 				Hello <span className="text-[#61DBFB] font-bold">React</span>
+				<Button>Test</Button>
 			</h1>
 		</div>
 	);
