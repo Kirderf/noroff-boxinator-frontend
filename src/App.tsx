b35fb8067112a1e153c6ca12d172f3c643ef2b09
Haukaun <haakonfs@hotmail.com>
2023-10-02 21:34:38 +0200

    Add: tanstack query for requests

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 		<div className="h-screen grid place-items-center">
 			<h1 className="text-8xl flex flex-col ">
 				Hello <span className="text-[#61DBFB] font-bold">React</span>
-				<Button>Test</Button>
+				<Button className="bg-slate-400 hover:bg-slate-600">Test</Button>
 			</h1>
 		</div>
 	);
