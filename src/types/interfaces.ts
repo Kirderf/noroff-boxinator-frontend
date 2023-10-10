4fd2065313f16525ad84ae58a40074357e19178f
Gard <gaf1902@outlook.com>
2023-10-10 15:05:17 +0200

    mb... sry håkon :(

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -5,3 +5,38 @@ interface Product {
   description: string;
   image: string;
 }
+interface Order {
+  id: number;
+  user: number;
+  products: Product[];
+  status: string;
+}
+
+interface Country {
+  id: number;
+  fullName: string;
+  shortName: string;
+  shippingCost: number;
+}
+
+interface User {
+  id: number;
+  address: string;
+  email: string;
+  roles: string;
+  username: string;
+}
+
+interface shipment {
+  id: number;
+  email: string;
+  destination: string;
+  billingAddress: string;
+  city: string;
+  postalCode: string;
+  phoneNumber: string;
+  deliveryInstruction: string;
+  country: Country;
+  order: Order;
+  gift: boolean;
+}
