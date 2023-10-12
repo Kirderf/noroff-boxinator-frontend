38dedf558e041221d5a95dee6dc06640fe5ab957
Gard <gaf1902@outlook.com>
2023-10-12 09:56:25 +0200

    style change on product description page

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -23,12 +23,16 @@ function ProductDetailPage() {
         if (!getProductByIdHook.isLoading)
             setProduct(getProductByIdHook.data as Product)
     }, [getProductByIdHook.isLoading])
-
+    const pdesEx = `:Contents:
+    Exploration gear for the rugged adventurer, including a thermos, pocket knife, compass, warm gloves, and a guidebook to Arctic wildlife.
+   
+   :Surprise Element: 
+   A collection of stunning Northern Lights photographs or a video message from a Scandinavian adventurer.`
     return (
         <main className="bg-primary-color h-auto flex justify-evenly w-full items-center ">
-            <div className="bg-background-color w-full max-w-[70rem] h-auto flex flex-col md:flex-row items-center justify-center rounded-lg text-primary-color p-4 m-10">
-                <div className="flex flex-col items-start mb-4 md:mb-0 w-full order-2">
-                    <img src={product?.image} alt={product?.name} className="object-cover max-w-[40rem] w-full h-[20rem]" />
+            <div className="bg-background-color max-w-[70rem] w-fit h-auto flex items-center rounded-lg text-primary-color p-4 m-10 flex-wrap justify-center">
+                <div className="flex items-start mb-4 md:mb-0 w-full order-2 max-w-[30rem] flex-wrap">
+                    <img src={product?.image} alt={product?.name} className="object-cover max-w-[40rem] w-full min-w-[15rem] h-[20rem]" />
                     <Table className="max-w-[40rem]">
                         <TableHeader>
                             <TableRow>
@@ -51,10 +55,18 @@ function ProductDetailPage() {
                         </TableBody>
                     </Table>
                 </div>
-                <div className="flex flex-col justify-between items-center max-w-md m-10">
+                <div className="flex flex-col justify-between items-center max-w-[25rem] m-10  ">
                     <div className="text-center mb-20">
                         <h1 className="text-2xl mb-2 font-bold">{product?.name}</h1>
-                        <p className="">{product?.description}</p>
+                        <p className="">{pdesEx.split(":").map((word) => {
+                            console.log(word)
+                            if (word == "Contents" || word == "Surprise Element") {
+                                return <span className=" text-green-color text-lg"><br />{word}:<br /></span>
+                            } else {
+                                return word + " "
+                            }
+
+                        })}</p>
                     </div>
                     <div className="flex gap-10 w-full mt-10 text-background-color">
                         <Button className="bg-accent-color-1 w-full font-bold" onClick={() => {
