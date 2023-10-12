2bc89fb0a5e2e827baaf8a9e9db5b35078972025
Gard <gaf1902@outlook.com>
2023-10-12 11:38:35 +0200

    added remove all from cart and price in dropdown

--- src/components/customComponents/dropDown/DropDownProducts.tsx
+++ src/components/customComponents/dropDown/DropDownProducts.tsx
@@ -10,17 +10,20 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux';
-import { decrementQuantity, incrementQuantity } from '../../../redux/chartSlice';
+import { decrementQuantity, incrementQuantity, removeAllItems } from '../../../redux/chartSlice';
+import { useNavigate } from "react-router-dom";
+
 
 const DropDownProducts = () => {
     const cart = useSelector((state: { product: Product, quantity: number }[]) => state)
     const dispatch = useDispatch()
+    const navigate = useNavigate()
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <img src="./icons/cart-icon.png" alt="Cart" />
             </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-56 bg-primary-color">
+            <DropdownMenuContent className="w-[27rem] bg-primary-color">
                 <DropdownMenuLabel>Products</DropdownMenuLabel>
                 <DropdownMenuSeparator />
                 <DropdownMenuGroup>
@@ -28,24 +31,34 @@ const DropDownProducts = () => {
                         <DropdownMenuItem key={item.product.id}>
                             <div className="flex flex-row gap-10" >
                                 <a href={"/product/" + item.product.id}>{item.product.name}</a>
-                                <div className="flex flex-row justify-center text-center content-center top-0 right-0" onClick={(e: any) => e.preventDefault()}>
-                                    <Button className="py-0 pb-5" onClick={() => {
-                                        dispatch(incrementQuantity(
-                                            item.product
-                                        ))
-                                    }}>+</Button>
-                                    <p>{item.quantity}</p>
-                                    <Button className="py-0 pb-5" onClick={() => {
-                                        dispatch(decrementQuantity(
-                                            item.product
-                                        ))
-                                    }}>-</Button>
+                                <div className="flex gap-4 flex-row justify-center text-center content-center top-0 right-0" onClick={(e: any) => e.preventDefault()}>
+                                    <p>Quantity:</p>
+                                    <div className="flex bg-accent-color-1 gap-2 px-5 rounded-xl">
+                                        <Button className="h-5 p-0" onClick={() => {
+                                            dispatch(incrementQuantity(
+                                                item.product
+                                            ))
+                                        }}>+</Button>
+                                        <p>{item.quantity}</p>
+                                        <Button className="h-5 p-0" onClick={() => {
+                                            dispatch(decrementQuantity(
+                                                item.product
+                                            ))
+                                        }}>-</Button>
+                                    </div>
+                                    <p>Price: {(item.product.price * item.quantity).toFixed(2)} NOK</p>
                                 </div>
                             </div>
                         </DropdownMenuItem>
-
                     ))}
                 </DropdownMenuGroup>
+                <div className="flex justify-between pt-5 px-1">
+                    <Button className=" bg-error-color right-0" onClick={() => {
+                        dispatch(removeAllItems())
+                    }}>Empty Cart</Button>
+                    <Button className="bg-accent-color-1 right-0" onClick={() => navigate("/checkout")}>Go to Checkout</Button>
+                </div>
+
             </DropdownMenuContent>
         </DropdownMenu>
     )
