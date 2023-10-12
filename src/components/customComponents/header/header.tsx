a34b46cd998f4ce8e2a3b325e4c40ddc30e71440
Gard <gaf1902@outlook.com>
2023-10-12 08:40:56 +0200

    made header sticky and changed div to header

--- src/components/customComponents/header/header.tsx
+++ src/components/customComponents/header/header.tsx
@@ -5,7 +5,7 @@ import DropDownProducts from "../dropDown/DropDownProducts"
 function Header() {
     const navigate = useNavigate()
     return (
-        <div className='w-full bg-primary-color p-5'>
+        <header className='w-full bg-primary-color p-5 sticky top-0'>
             <div className='w-[90%] mx-auto flex justify-between gap-5'>
                 <a onClick={() => navigate("/")}>
                     <h1 className='text-[2rem] font-bold text-green-color'>Boxinator</h1>
@@ -22,7 +22,7 @@ function Header() {
                 </div>
 
             </div>
-        </div>
+        </header>
     )
 }
 
