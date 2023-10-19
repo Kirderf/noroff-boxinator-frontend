cd1957ecf9fc2cd3942b2a0249433cac80771129
Haukaun <haakonfs@hotmail.com>
2023-10-19 10:24:39 +0200

    patch product workign

--- src/services/product/productPatch.ts
+++ src/services/product/productPatch.ts
@@ -9,8 +9,8 @@ export async function updateProduct(
   try {
     console.log({
       id: product?.id,
-      price: productValues?.price,
-      stock: productValues?.stock,
+      price: product?.price,
+      stock: product?.stock,
       description: productValues?.description,
       name: productValues?.name,
       image: product?.image,
@@ -21,31 +21,31 @@ export async function updateProduct(
       active: product?.active,
     });
     const response = await fetch(
-      "https://boxinator2.azurewebsites.net/api/v1/product" +
-        {
-          method: "PATCH",
-          body: JSON.stringify({
-            id: product?.id,
-            price: productValues?.price,
-            stock: productValues?.stock,
-            description: productValues?.description,
-            name: productValues?.name,
-            image: product?.image,
-            width: product?.width,
-            height: product?.height,
-            depth: product?.depth,
-            weight: product?.weight,
-            active: product?.active,
-          }),
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            authorization: "bearer " + token,
-          },
-        }
+      "https://boxinator2.azurewebsites.net/api/v1/product",
+      {
+        method: "PATCH",
+        body: JSON.stringify({
+          id: product?.id,
+          price: productValues?.price,
+          stock: productValues?.stock,
+          description: productValues?.description,
+          name: productValues?.name,
+          image: product?.image,
+          width: product?.width,
+          height: product?.height,
+          depth: product?.depth,
+          weight: product?.weight,
+          active: product?.active,
+        }),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          authorization: "bearer " + token,
+        },
+      }
     );
     if (!response.ok) {
-      throw new Error("Failed to update Product");
+      throw new Error("Failed to update Shipment");
     }
     const data = await response.json();
     return data;
