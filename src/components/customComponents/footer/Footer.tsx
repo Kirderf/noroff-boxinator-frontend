13231022308fff245e69ef1d277ba4f3b47961b6
Gard <gaf1902@outlook.com>
2023-10-20 12:24:25 +0200

    mobile changes

--- src/components/customComponents/footer/Footer.tsx
+++ src/components/customComponents/footer/Footer.tsx
@@ -1,14 +1,14 @@
 function Footer() {
     return (
         <footer className=" w-full py-10 px-4 bg-primary-color text-center relative">
-            <div className="flex flex-col md:flex-row justify-center items-center">
-                <div className=" absolute left-10">
+            <div className="flex flex-col min-[350px]:flex-row justify-between  items-center">
+                <div className="">
                     <a className="flex-none text-xl font-semibold text-black dark:text-white" href="/" aria-label="Brand">
-                        <img src="/./images/BoxinatorLogo.png" alt="Boxinator Logo" className="w-32" />  {/* Assuming you have a logo image */}
+                        <img src="/./images/BoxinatorLogo.png" alt="Boxinator Logo" className="w-32" />
                     </a>
                 </div>
                 <nav className="">
-                    <ul className="flex justify-center space-x-4">
+                    <ul className="">
                         <li><a href="/contact" className="text-black dark:text-white hover:text-gray-700 dark:hover:text-gray-300">Contact us</a></li>
                     </ul>
                     <div className="mt-4">
