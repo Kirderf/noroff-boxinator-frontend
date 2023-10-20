96bef996b2b0a3436b9dc084dca6e13043fd6001
Gard <gaf1902@outlook.com>
2023-10-20 09:03:25 +0200

    temp fix on keycloak user add to db

--- src/services/keycloak/keyclokAdapter.ts
+++ src/services/keycloak/keyclokAdapter.ts
@@ -6,6 +6,44 @@ import Keycloak from "keycloak-js";
 function useKeyCloak() {
    
   const [keycloak, setKeycloak] = useState<Keycloak>();
+  useEffect(() => {
+    if(keycloak?.authenticated){
+      post()
+    }
+  },[keycloak?.token])
+
+
+  const post = async () => {
+    const user = (await keycloak?.loadUserInfo()) as any;
+    console.log(user.sub)
+    const u: User = {
+      id: user.sub,
+      username: user.name,
+      address: "",
+      email: user.email,
+      roles: "",
+    };
+    try {
+      await fetch("https://boxinator2.azurewebsites.net/api/v1/user", {
+        method: "POST",
+        //  mode: 'no-cors',
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          authorization: "bearer " + keycloak?.token,
+        },
+        body: JSON.stringify(u),
+      }).then((data) => {
+        if (data?.ok) {
+          console.log("user made in db!");
+        } else {
+          console.log("error");
+        }
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
   //TODO må va ein annæ måte :/
   useEffect(() => {
     var _keycloak = new Keycloak({
@@ -21,42 +59,6 @@ function useKeyCloak() {
       .then(() => {
         setKeycloak(_keycloak);
       });
-
-    const post = async () => {
-      const user = (await keycloak?.loadUserInfo()) as any;
-
-      const u: User = {
-        id: user.sub,
-        username: user.name,
-        address: "",
-        email: user.email,
-        roles: "",
-      };
-      try {
-        await fetch("https://boxinator2.azurewebsites.net/api/v1/user", {
-          method: "POST",
-          //  mode: 'no-cors',
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            authorization: "bearer " + keycloak?.token,
-          },
-          body: JSON.stringify(u),
-        }).then((data) => {
-          if (data?.ok) {
-            console.log("user made in db!");
-          } else {
-            console.log("error");
-          }
-        });
-      } catch (e) {
-        console.log(e);
-      }
-    };
-    if (keycloak?.tokenParsed) {
-      console.log("hei")
-      post();
-    }
   }, []);
 
   return keycloak;
