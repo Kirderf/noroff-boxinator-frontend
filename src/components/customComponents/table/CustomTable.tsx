2358c0a9d85635934499156274046fd50869c05f
Haukaun <haakonfs@hotmail.com>
2023-10-20 13:09:05 +0200

    fix: profile table

--- src/components/customComponents/table/CustomTable.tsx
+++ src/components/customComponents/table/CustomTable.tsx
@@ -22,10 +22,10 @@ export function CustomTable(props: CustomTableProps) {
             <TableCaption>Order history</TableCaption>
             <TableHeader >
                 <TableRow className="bg-accent-color-2">
-                    <TableHead className="w-[100px]">Id</TableHead>
+                    <TableHead className="">Id</TableHead>
                     <TableHead>Status</TableHead>
                     <TableHead>ProductQuantity</TableHead>
-                    <TableHead className="text-right">Amount</TableHead>
+                    <TableHead className="">User</TableHead>
                     <TableHead className="text-center">Action</TableHead>
                 </TableRow>
             </TableHeader>
@@ -35,8 +35,8 @@ export function CustomTable(props: CustomTableProps) {
                         <TableCell className="font-medium">{shipment?.id}</TableCell>
                         <TableCell>{shipment?.status}</TableCell>
                         <TableCell>{shipment.shipmentProducts?.length}</TableCell>
-                        <TableCell className="text-right">{shipment?.user}</TableCell>
-                        <TableCell className="text-center w-[10rem]">{<ShipmentDialog {...shipment} />} </TableCell>
+                        <TableCell className="">{shipment?.user}</TableCell>
+                        <TableCell className="">{<ShipmentDialog {...shipment} />} </TableCell>
                     </TableRow>
                 ))}
             </TableBody>
