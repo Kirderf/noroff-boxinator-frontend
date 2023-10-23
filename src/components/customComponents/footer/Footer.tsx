ec84b630c742b709e3f1237dcc73155ca1be0fa8
Gard <gaf1902@outlook.com>
2023-10-23 14:12:37 +0200

    div center

--- src/components/customComponents/footer/Footer.tsx
+++ src/components/customComponents/footer/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <footer className=" w-full py-10 px-4 bg-primary-color text-center relative">
-            <div className="flex flex-col min-[350px]:flex-row justify-between  items-center">
+            <div className="flex flex-col min-[350px]:flex-row justify-between items-center">
                 <div className="">
                     <a className="flex-none text-xl font-semibold text-black dark:text-white" href="/" aria-label="Brand">
                         <img src="/./images/BoxinatorLogo.png" alt="Boxinator Logo" className="w-32" />
@@ -11,13 +11,15 @@ function Footer() {
                     <ul className="">
                         <li><a href="/contact" className="text-black dark:text-white hover:text-gray-700 dark:hover:text-gray-300">Contact us</a></li>
                     </ul>
-                    <div className="mt-4">
+                    <div className="mt-4 flex justify-start">
                         <p className="text-xs text-black dark:text-white">
                             &copy; {new Date().getFullYear()} Boxinator. All Rights Reserved.
                         </p>
-                    </div>
-                </nav>
 
+                    </div>
+
+                </nav>
+                <div className="w-32"></div>
             </div>
 
         </footer>
