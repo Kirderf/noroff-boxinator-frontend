12e5ecf5843ba05fe4a7e5b1f95b4a0e247382f4
Fredrik Holm Julsen <Fredrik.julsen71@gmail.com>
2023-10-23 13:28:17 +0200

    header fix

--- src/components/customComponents/header/header.tsx
+++ src/components/customComponents/header/header.tsx
@@ -15,14 +15,15 @@ function Header() {
                 <a onClick={() => navigate("/")}>
                     <h1 className='hover:animate-pop-up text-[1.7rem] font-bold text-green-color hover:cursor-pointer'>Boxinator</h1>
                 </a>
-                <div className="flex gap-7 items-center">
-                    <a className="hover:cursor-pointer">
+                <div className="flex gap-5 items-center">
+                    <a className="hover:cursor-pointer order-1">
                         <DropDownProducts />
                     </a>
                     {
                         keycloak.keycloak?.authenticated ? (
-                            <div className="flex gap-7 items-center">
-                                <a onClick={() => navigate("/profile")} className="hover:cursor-pointer">
+                            <div className="hover:animate-pop-up">
+                                <a onClick={() => navigate("/profile")} className="hover:cursor-pointer flex  gap-4 items-center">
+                                    <h1 className="text-lg ">PROFILE</h1>
                                     <Avatar className="border">
                                         <AvatarImage src="../images/freddy.png" alt="@shadcn" />
                                         <AvatarFallback>{keycloak.keycloak.profile?.username?.substring(0, 1)}</AvatarFallback>
@@ -32,7 +33,7 @@ function Header() {
                         ) : (
                             <div className="flex gap-7 items-center hover:animate-pop-up">
                                 <Button onClick={() => keycloak.keycloak?.login()}>
-                                    <img src="/./icons/loginicon.svg" alt="Login" />
+                                    <h1 className="text-lg">LOGIN</h1>
                                 </Button>
                             </div>
                         )
